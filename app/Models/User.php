1f0922c1d28116273fee6bbe8cdef7f6b7e5cbcc
AyoubMS <chahayb563@gmail.com>
2023-08-07 14:36:08 +0200

    add one to one relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,16 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    // $user->profile
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    public function experience()
+    {
+        return $this->hasOne(Experience::class);
+    }
 }
