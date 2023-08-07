19160fae3508e709d47560574bd567d7a6dd4afb
AyoubMS <chahayb563@gmail.com>
2023-08-07 19:06:38 +0200

    add polymorphic relations

---
+++ app/Models/Series.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Series extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function videos()
+    {
+        return $this->morphMany(Video::class, 'watchable');
+    }
+}
