efb70dc0e133281b289cfb31b2ae7ff5521cee63
AyoubMS <chahayb563@gmail.com>
2023-08-07 17:35:30 +0200

    add has many through relationship

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Affiliation;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -23,7 +24,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'experience_points' => 0
+            'experience_points' => 0,
+            'affiliation_id' => Affiliation::factory()
         ];
     }
 
